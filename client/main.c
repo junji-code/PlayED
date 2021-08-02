cd8720ec0c7a4355fb4498fc1cfcaf5119f35acc
Viniciusmj <vinijaguare@gmail.com>
2021-08-02 14:49:41 -0300

    Playlist implementada

--- client/main.c
+++ client/main.c
@@ -7,36 +7,7 @@
 
 
 int main(){
-    tList* playlist = InitPlaylist();
-    char arq1[] = "../Entrada/Entrada/heavymetal.txt";
-    char arq2[] = "../Entrada/Entrada/acoustic-hits.txt";
-    addPlaylist(playlist, arq1);
-    addPlaylist(playlist, arq2);
-
-    PrintList(playlist, ImpreimePlaylist);
-
-    DestroyList(playlist);
-
-    // tList* Musicas = LeArquivoMusicas("../Entrada/Entrada/acoustic-hits.txt");
-    // char aux[] = "Alicia Keys - No One - Acoustic";
-
-    // PrintList(Musicas, ImprimeMusica);
-    // printf("\n\n");
-
-    // SearchList(Musicas, aux, cmpMusica);
-    // PrintList(Musicas, ImprimeMusica);
-    // printf("\n\n");
-
-    // void* koe;
-    // koe = SearchRemoveList(Musicas, aux, cmpMusica);
-    // ImprimeMusica(koe);
-    // printf("\n\n");
     
-    // destroyMusica(koe);
-    // free(koe);
-    // PrintList(Musicas, ImprimeMusica);
-    // DestroyList(Musicas);
-
 
 return 0;
 }
