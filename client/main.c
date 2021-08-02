c96da50fc7010ceea0e54ec503f3660965e9e691
Christian Junji <71985188+junji-code@users.noreply.github.com>
2021-08-02 16:11:17 -0300

    Add files via upload

---
+++ client/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/Musica.h"
+<<<<<<< HEAD
+#include "../include/Usuario.h"
+=======
+#include "../include/Playlist.h"
+>>>>>>> ecce89718658e82a9920026cf137c76047707143
+#include "../include/Lista.h"
+#include <string.h>
+
+int main()
+{
+    char aux[] = "Entrada/amizade.txt";
+
+<<<<<<< HEAD
+    tList *usuarios = inicializaUsuarios(aux);
+
+    PrintList(usuarios, imprimeAmigos);
+
+    return 0;
+=======
+int main(){
+    
+
+return 0;
+>>>>>>> ecce89718658e82a9920026cf137c76047707143
+}
