e7dfd6b8ad45fcff9fc064d0a6fc9631f33d9ef3
junji-code <junjao.ccomp@gmail.com>
2021-08-05 14:26:12 -0300

    nada

---
+++ client/main.c
@@ -0,0 +1,12 @@
+#include "../include/Lista.h"
+#include "../include/Usuario.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    tList *usuarios = inicializaUsuarios("Entrada/amizade.txt");
+
+    return 0;
+}
